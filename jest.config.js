e62f822171ff8359a0fffd75bca905cd37debf0e
hubiqing <hbqhbq123@qq.com>
2020-09-16 10:51:50 +0800

    ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testPathIgnorePatterns: ['/node_modules/'],
   testEnvironment: 'node',
   roots: ['__tests__'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   resetModules: true,
   clearMocks: true,
   collectCoverage: true,
